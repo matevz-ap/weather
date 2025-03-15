8344195fad2a04c5e334bf48e73a76f3ab9677d7
matevz-ap <47795510+matevz-ap@users.noreply.github.com>
2025-03-15 22:19:45 +0100

    Add python types

--- test/main.py
+++ test/main.py
@@ -3,40 +3,45 @@ import pika
 import json
 import uuid
 import requests
+from pika.adapters.blocking_connection import BlockingChannel
 from requests.auth import HTTPBasicAuth
 
 app = Flask(__name__)
 
-connection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq"))
-channel = connection.channel()
+connection: pika.BlockingConnection = pika.BlockingConnection(
+    pika.ConnectionParameters("rabbitmq")
+)
+channel: BlockingChannel = connection.channel()
 
 
 @app.route("/weather/<location>")
-def weather(location: str):
-    corr_id = str(uuid.uuid4())
+def weather(location: str) -> str:
+    corr_id: str = str(uuid.uuid4())
     channel.basic_publish(
         exchange="weather",
         routing_key="",
         properties=pika.BasicProperties(
             correlation_id=corr_id,
         ),
-        body=location,
+        body=location.encode(),
     )
     return corr_id
 
 
 @app.route("/weather/results/<uuid>")
-def weather_results(uuid: str):
-    method_frame, header_frame, body = channel.basic_get(queue=uuid, auto_ack=True)
+def weather_results(uuid: str) -> Response | dict:
+    _, _, body = channel.basic_get(queue=uuid, auto_ack=True)
     if body is None:
         return Response("Page not found", status=404)
     return json.loads(body)
 
 
 @app.route("/metrics")
-def metrics():
+def metrics() -> dict:
     auth = HTTPBasicAuth("guest", "guest")
-    response = requests.get("http://rabbitmq:15672/api/overview", auth=auth)
+    response: requests.Response = requests.get(
+        "http://rabbitmq:15672/api/overview", auth=auth
+    )
     return response.json()
 
 
